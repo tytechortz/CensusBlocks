36d2bcdc3af2b8604359e697e6f9f5396a3a186f
James Swank <jmswank7@gmail.com>
2023-05-28 20:49:57 -0600

    undu blocks

---
+++ utils.py
@@ -0,0 +1,22 @@
+import geopandas as gpd
+import pandas as pd
+
+
+
+def get_svi_data():
+    df = pd.read_csv('Colorado_SVI_2020.csv')
+    geo_data = gpd.read_file('2020_CT/ArapahoeCT.shp')
+
+    df['FIPS'] = df['FIPS'].astype(str)
+    df = df.set_index("FIPS")
+    df = geo_data.merge(df, on="FIPS")
+
+    return df
+
+
+
+def get_geo_data():
+    geo_data = gpd.read_file('tabblock20/tl_2020_08_tabblock20.shp')
+    # print(geo_data)
+
+    return geo_data
