73d23b035c527df35a3e7423252e6f2bf3fdff80
James Swank <jmswank7@gmail.com>
2023-06-05 18:19:15 -0600

    cleanup

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def get_block_group_data():
     # df = pd.read_csv('DECENNIALDHC2020.P1-Data.csv')
-    df1 = pd.read_csv('BlockGroupPop.csv', dtype=str)
+    df1 = pd.read_csv('2020Census_BGPop.csv')
     # geo_data = gpd.read_file('Census_Blocks_2020_SHAPE_WGS/Census_Blocks_2020_WGS.shp')
     geo_data = gpd.read_file('tl_2022_08_bg (1)/tl_2022_08_bg.shp')
    
@@ -39,27 +39,17 @@ def get_block_data():
 
 def get_tract_data():
     df1 = pd.read_csv('TractPop.csv')
-    print(df1.columns)
-    print(df1['GEOID'])
     
     geo_data = gpd.read_file('2020_CT/ArapahoeCT.shp')
     geo_data = geo_data.rename(columns={'FIPS':'GEOID'})
-    print(geo_data.columns)
-    print(geo_data['GEOID'])
+  
     geo_data['GEOID'] = geo_data['GEOID'].astype(int)
     df1['Total'] = df1['Total'].str.replace(',', '').astype(int)
-    print(geo_data.dtypes)
-    print(df1.dtypes)
-    # df1['GEOID20'] = df1['GEO_ID'].apply(lambda x: x[9:])
     
-    # df1.columns.values[2] = 'Total'
-    
-    # geo_arap = geo_data[geo_data['COUNTYFP20'] == "005"]
    
     df = geo_data.merge(df1, on="GEOID")
    
     
-
     return df
 
 
