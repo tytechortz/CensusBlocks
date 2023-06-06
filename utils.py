87d8e0ff23ddacd862a41233702e014cc636ef13
James Swank <jmswank7@gmail.com>
2023-06-05 18:40:14 -0600

    cleanup

--- utils.py
+++ utils.py
@@ -4,16 +4,15 @@ import pandas as pd
 
 
 def get_block_group_data():
-    # df = pd.read_csv('DECENNIALDHC2020.P1-Data.csv')
+   
     df1 = pd.read_csv('BlockGroupPop.csv')
-    # geo_data = gpd.read_file('Census_Blocks_2020_SHAPE_WGS/Census_Blocks_2020_WGS.shp')
+   
     geo_data = gpd.read_file('tl_2022_08_bg (1)/tl_2022_08_bg.shp')
    
     geo_arap = geo_data[geo_data['COUNTYFP'] == "005"]
    
     df1['Total'] = df1['Total'].str.replace(',', '').astype(int)
-    # df1['GEOID'] = df1['GEOID'].astype(int)
-    print(df1.columns)
+   
     geo_data['GEOID'] = geo_data['GEOID'].astype(int)
     df = geo_data.merge(df1, on="GEOID")
     df.to_csv("GEOID.csv")
